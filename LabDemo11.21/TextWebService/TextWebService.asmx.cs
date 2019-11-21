7c7dae96d3efbc1a5781490546188042e60d7bc4
Regina Pilipchuk <regina.pilipchuk@gmail.com>
2019-11-21 14:08:35 -0800

    Lab complete - 11.21.2019

---
+++ LabDemo11.21/TextWebService/TextWebService.asmx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace TextWebService
+{
+    /// <summary>
+    /// Summary description for TextWebService
+    /// </summary>
+    [WebService(Namespace = "http://microsoft.com/webservices/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class TextWebService : System.Web.Services.WebService
+    {
+        // this adorement is required in order for this to be a web service method
+        // this allows for the method to be advertised to the "world"
+        // you are able to have internal methods too
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
